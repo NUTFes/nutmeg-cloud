5dbe7fb77afff17a2e4cc35d168122de8b240f8a
nose221834 <iatyy01151821@gmail.com>
2024-11-30 18:06:24 +0900

    削除

--- server/proxmox/provider.tf
+++
@@ -1,7 +0,0 @@
-# Terraformのプロバイダ設定
-provider "proxmox" {
-  pm_api_url     = "https://your-proxmox-server/api2/json"
-  pm_user        = "your-username@pam"
-  pm_password    = "your-password"
-  insecure_https = false
-}
